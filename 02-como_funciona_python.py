d1f98970f730c11002c5be7e4c84ac64cde4595c
abrahambtz <abrahambz@outlook.com>
2021-10-09 16:27:47 -0500

    Funcionamiento de python

---
+++ 02-como_funciona_python.py
@@ -0,0 +1,19 @@
+def run():
+    how_does = '''
+    Python es un lenguaje interpretado. 
+    En python nuestro codigo no pasa directamente a lenguaje maquina 0,1.
+
+    En python nuestro codigo es interpretado en una (maquina virtual) y esta maquina virtual 
+    convierte tu codigo en bytecode.
+
+    El bytecode es un código intermedio más abstracto que el código máquina. Habitualmente,
+     es tratado como un archivo binario que contiene un programa ejecutable similar a un 
+     módulo objeto, que es un archivo binario producido por el compilador cuyo contenido 
+     es el código objeto o código máquina .
+    
+    
+    
+    
+    '''
+if __name__ == '__main__':
+    run()
